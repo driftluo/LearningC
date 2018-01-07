142035c802a13c9006ee2b299316ae25ca9d8982
piaoliu <441594700@qq.com>
2018-01-07 22:42:23 +0800

    4.3-4.4

---
+++ The_C_Programing_Language/Chapter_4/exercise_4.3.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h> /* for atof() */
+#include <ctype.h>
+
+/*
+Given the basic framework, it's straightforward to extend the calculator. Add
+the modulus (%) operator and provisions for negative numbers.
+*/
+
+#define MAXOP 100   /* max size of operand or operator */
+#define NUMBER '0'  /* signal that a number was found */
+#define MAXVAL 100 /* maximum depth of val stack */
+#define BUFSIZE 100
+
+int sp = 0;         /* next free stack position */
+double val[MAXVAL]; /* value stack */
+char buf[BUFSIZE];  /* buffer for ungetch */
+int bufp = 0;       /* next free position in buf */
+
+int getop(char []);
+void push(double);
+double pop(void);
+int getch(void);
+void ungetch(int);
+
+/* reverse Polish calculator */
+int main() {
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case '%':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push((int) pop() % (int) op2);
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("error: unknown commad %s\n", s);
+                break;
+        }
+    }
+    return 0;
+}
+
+/* push: push f onto value stack */
+void push(double f) {
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
+}
+
+/* pop: pop and return top value from stack */
+double pop(void) {
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+/* getop: get next character or numeric operand */
+int getop(char s[]) {
+    int i = 0, c, next;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+
+    if (!isdigit(c) && c != '.' && c != '-')
+        return c;           /* not a number */
+
+    if (c == '-') {
+        next = getch();
+        if (!isdigit(next) && next != '.')
+            return c;       /* not a number */
+        c = next;
+        s[i] = '-';
+    } else {
+        c = getch();
+    }
+       
+    while (isdigit(s[++i] = c))    /* collect integer part */
+        c = getch();
+    if (c == '.')   /* collect fraction part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+
+/* get a (possibly pushed-back) character */
+int getch(void) {
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+/* push character back on input */
+void ungetch(int c) {
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
