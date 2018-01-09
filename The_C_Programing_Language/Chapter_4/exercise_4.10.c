2c576b6756f685f904ad1ed380371d692222ae5c
piaoliu <441594700@qq.com>
2018-01-09 20:22:47 +0800

    4.10

--- The_C_Programing_Language/Chapter_4/exercise_4.10.c
+++ The_C_Programing_Language/Chapter_4/exercise_4.10.c
@@ -1,8 +1,124 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 /*
 An alternate organization uses  getline to read an entire input line; this makes
 getch and  ungetch unnecessary. Revise the calculator to use this approach.
 */
 
+#define MAXLINE 100
+#define NUMBER '0'
+#define MAXOP 100
+#define MAXVAL 100
+
+int li = 0, sp = 0;
+char line[MAXLINE];
+char val[MAXLINE];
+
+int getline(char line[], int max);
+int getop(char s[]);
+void push(double);
+double pop(void);
+
+/* reverse Polish calculator */
+int main() {
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case '%':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push((int) pop() % (int) op2);
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("error: unknown commad %s\n", s);
+                break;
+        }
+    }
+    return 0;
+}
+
+int getop(char s[]) {
+    int c, i = 0;
+
+    if (line[li] == '\0')
+        if (getline(line, MAXLINE) == 0)
+            return EOF;
+        else
+            li = 0;
+
+    while ((s[0] = c = line[li++]) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+        return c;
+    if(isdigit(c))
+        while (isdigit(s[++i] = c = line[li++]))
+            ;
+    if (c == '.')
+        while (isdigit(s[++i] = c = line[li++]))
+            ;
+    s[i] = '\0';
+    // li--;
+    return NUMBER;
+}
+
+// getline: get line into s, return length
+int getline(char s[], int lim) {
+    int c, i;
+
+    i = 0;
+    while (--lim > 0 && (c=getchar()) != EOF && c != '\n')
+        s[i++] = c;
+    if (c = '\n')
+        s[i++] = c;
+    s[i] = '\0';
+    return i;
+}
+
+/* push: push f onto value stack */
+void push(double f) {
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
+}
+
+/* pop: pop and return top value from stack */
+double pop(void) {
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
